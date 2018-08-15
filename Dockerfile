d1a23b2283fb34cda3db94a4311f37496c71d7a4
gsmlg <me@gsmlg.org>
2018-08-15 23:08:15 +0800

    update config

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,17 @@ FROM alpine:3.8
 
 MAINTAINER GSMLG < me@gsmlg.org >
 
+ENV SERVER_MODE=master \
+    SERVER_URL="" \
+    PRIVATE_KEY=/etc/stunnel/pkey.pem \
+    CERTIFICATE=/etc/stunnel/cert.pem
+
 RUN apk update \
     && apk add curl \
     && apk add stunnel \
     && rm -rf /var/cache/apk/*
 
-COPY stunnel.conf stunnel-client.conf pkey.pem cert.pem /etc/stunnel/
+COPY pkey.pem cert.pem /etc/stunnel/
 
 EXPOSE 80 443
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,73 @@ STUNNEL=$(/usr/bin/which stunnel)
 mkdir -p /run/stunnel
 "$CHOWN" -R stunnel:stunnel /run/stunnel
 
-if test "$1" "==" "client"
+SERVER_MODE=${SERVER_MODE:-master}
+SERVER_URL=${SERVER_URL}
+PRIVATE_KEY=${PRIVATE_KEY:-/etc/stunnel/pkey.pem}
+CERTIFICATE=${CERTIFICATE:-/etc/stunnel/cert.pem}
+
+CONF=/etc/stunnel/stunnel.conf
+
+if test "$SERVER_MODE" "==" "master"
 then
-    CONF=/etc/stunnel/stunnel-client.conf
-    REMOTE_HOST=${REMOTE_HOST:-proxy}
-    sed -i'' "s/REMOTE_HOST/${REMOTE_HOST}/g" "$CONF"
+  # server mode
+  cat <<EOF > "${CONF}"
+# Certificate/key is needed in server mode
+cert = $CERTIFICATE
+key = $PRIVATE_KEY
+
+# Some security enhancements for UNIX systems - comment them out on Win32
+# chroot = /chroot/stunnel/
+setuid = stunnel
+setgid = stunnel
+
+# PID is created inside chroot jail
+pid = /run/stunnel/stunnel.pid
+
+# foreground
+foreground = yes
+
+# Some performance tunings
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+#compression = rle
+
+[https]
+accept = 443
+connect = squid:3128
+TIMEOUTclose = 0
+
+EOF
 else
-    CONF=/etc/stunnel/stunnel.conf
+  # client mode
+  cat <<EOF > "${CONF}"
+# Certificate/key is optional in client mode
+cert = $CERTIFICATE
+key = $PRIVATE_KEY
+
+# Some security enhancements for UNIX systems - comment them out on Win32
+# chroot = /chroot/stunnel/
+setuid = stunnel
+setgid = stunnel
+
+# PID is created inside chroot jail
+pid = /run/stunnel/stunnel.pid
+
+# foreground
+foreground = yes
+
+# Some performance tunings
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+#compression = rle
+
+[https]
+accept = 80
+connect = $SERVER_URL
+TIMEOUTclose = 0
+verifyChain = no
+
+EOF
 fi
 
 # start stunnel in foreground
